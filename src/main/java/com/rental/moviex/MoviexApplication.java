e049e125d11f6447d1d7066ea7599810fb774a97
zuluwarrior147 <todlhmw@gmail.com>
2020-07-25 11:24:02 +0200

    setted up project structure

---
+++ src/main/java/com/rental/moviex/MoviexApplication.java
@@ -0,0 +1,13 @@
+package com.rental.moviex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviexApplication.class, args);
+	}
+
+}

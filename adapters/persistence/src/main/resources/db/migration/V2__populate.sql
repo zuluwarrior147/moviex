2597613782fbffdccceebdbb3c77887a0229b6c7
zuluwarrior147 <todlhmw@gmail.com>
2020-07-26 12:20:36 +0200

    added populate script

---
+++ adapters/persistence/src/main/resources/db/migration/V2__populate.sql
@@ -0,0 +1,26 @@
+INSERT INTO users(id, email, first_name, last_name, date_of_birth, bonus_points, created_at, last_modified)
+VALUES(121, 'martin.scorsese@mails.com', 'Martin', 'Scorsese', '1942-10-17', 12, current_timestamp, current_timestamp);
+
+INSERT INTO users(id, email, first_name, last_name, date_of_birth, bonus_points, created_at, last_modified)
+VALUES(122, 'darren.aronofsky@mails.com', 'Darren', 'Aronofsky', '1969-02-12', 7, current_timestamp, current_timestamp);
+
+INSERT INTO users(id, email, first_name, last_name, date_of_birth, bonus_points, created_at, last_modified)
+VALUES(132, 'gaspar.noe@mails.com', 'Gaspar', 'Noe', '1963-12-27', 13, current_timestamp, current_timestamp);
+
+INSERT INTO users(id, email, first_name, last_name, date_of_birth, bonus_points, created_at, last_modified)
+VALUES(131, 'ridley.scott@mails.com', 'Ridley', 'Scott', '1937-10-30', 5, current_timestamp, current_timestamp);
+
+INSERT INTO movie(id, title, type, duration, release_date, created_at, last_modified)
+VALuES(111, 'Climax', 1, 5820000, '2018-12-06', current_timestamp, current_timestamp);
+
+INSERT INTO movie(id, title, type, duration, release_date, created_at, last_modified)
+VALuES(121, 'The Departed', 2, 9060000, '2006-09-26', current_timestamp, current_timestamp);
+
+INSERT INTO movie(id, title, type, duration, release_date, created_at, last_modified)
+VALuES(123, 'The Fountain', 2, 5760000, '2006-01-18', current_timestamp, current_timestamp);
+
+INSERT INTO movie(id, title, type, duration, release_date, created_at, last_modified)
+VALuES(112, 'Blade Runner', 2, 7020000, '1982-06-25', current_timestamp, current_timestamp);
+
+INSERT INTO movie(id, title, type, duration, release_date, created_at, last_modified)
+VALuES(113, 'Black Swan', 1, 6480000, '2011-01-20', current_timestamp, current_timestamp);
